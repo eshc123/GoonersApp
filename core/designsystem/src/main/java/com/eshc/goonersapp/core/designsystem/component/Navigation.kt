fa994fedc70dbf22f3cb25463ea5986f2d260a78
eshc123 <eshc@naver.com>
2023-11-09 21:24:54 +0900

    designSystem Navigation 수정

--- core/designsystem/src/main/java/com/eshc/goonersapp/core/designsystem/component/Navigation.kt
+++ core/designsystem/src/main/java/com/eshc/goonersapp/core/designsystem/component/Navigation.kt
@@ -1,14 +1,26 @@
 package com.eshc.goonersapp.core.designsystem.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material3.NavigationBar
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun RowScope.GoonersNavigationBarItem(
+fun RowScope.GnrNavigationBarItem(
     selected : Boolean,
     onClick : () -> Unit,
     icon : @Composable () -> Unit,
@@ -24,13 +36,28 @@ fun RowScope.GoonersNavigationBarItem(
 }
 
 @Composable
-fun GoonersNavigationBar(
+fun GnrNavigationBar(
     modifier: Modifier = Modifier,
+    containerColor: Color = NavigationBarDefaults.containerColor,
+    contentColor: Color = MaterialTheme.colorScheme.contentColorFor(containerColor),
+    tonalElevation: Dp = NavigationBarDefaults.Elevation,
+    windowInsets: WindowInsets = NavigationBarDefaults.windowInsets,
     content: @Composable RowScope.() -> Unit
 ) {
-    NavigationBar(
-        modifier = modifier,
-        tonalElevation = 0.dp,
-        content = content,
-    )
+    Surface(
+        color = containerColor,
+        contentColor = contentColor,
+        tonalElevation = tonalElevation,
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(windowInsets)
+                .height(40.dp)
+                .selectableGroup(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            content = content
+        )
+    }
 }

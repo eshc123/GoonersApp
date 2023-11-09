a91fe38385b8afe1d9897312c4f672c68f2d4442
eshc123 <eshc@naver.com>
2023-11-09 21:24:09 +0900

    designSystem resource 추가

---
+++ core/designsystem/src/main/java/com/eshc/goonersapp/core/designsystem/theme/Font.kt
@@ -0,0 +1,13 @@
+package com.eshc.goonersapp.core.designsystem.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.eshc.goonersapp.core.designsystem.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+)

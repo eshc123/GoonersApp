d09341153fe1dab466a4adc4200a92e584e749a5
eshc123 <eshc@naver.com>
2023-11-09 21:44:33 +0900

    team navigation 추가

---
+++ feature/team/src/main/java/com/eshc/goonersapp/feature/team/TeamScreen.kt
@@ -0,0 +1,17 @@
+package com.eshc.goonersapp.feature.team
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TeamRoute(){
+    TeamScreen()
+}
+
+@Composable
+fun TeamScreen() {
+    Column() {
+
+    }
+
+}

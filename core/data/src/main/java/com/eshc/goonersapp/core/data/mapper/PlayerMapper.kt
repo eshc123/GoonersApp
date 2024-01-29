0ed0d9866a49f90b7987ee16cc532c044cca7af9
Jung SeokJoon <eshc.joon@gmail.com>
2024-01-30 06:50:48 +0900

    mod PlayerMapper

--- core/data/src/main/java/com/eshc/goonersapp/core/data/mapper/PlayerMapper.kt
+++ core/data/src/main/java/com/eshc/goonersapp/core/data/mapper/PlayerMapper.kt
@@ -1,5 +1,6 @@
 package com.eshc.goonersapp.core.data.mapper
 
+import com.eshc.goonersapp.core.database.model.PlayerEntity
 import com.eshc.goonersapp.core.domain.model.Player
 import com.eshc.goonersapp.core.network.model.RemotePlayer
 
@@ -16,5 +17,33 @@ fun RemotePlayer.toModel() = Player(
     positionDetail = mainPosition,
     position = positionInitial,
     positionCategory = positionCategory,
+    nationality = nationality
+)
+
+fun RemotePlayer.toEntity() = PlayerEntity(
+    id = id,
+    name = name,
+    backNumber = backNumber,
+    birthDate = birthDate,
+    height = height,
+    weight = weight,
+    imageUrl = imageUrl,
+    contractStartDate = contractStartDate,
+    contractEndDate = contractEndDate,
+    mainPosition = mainPosition,
+    nationality = nationality
+)
+
+fun PlayerEntity.toModel() = Player(
+    id = id,
+    name = name,
+    backNumber = backNumber,
+    birthDate = birthDate,
+    height = height,
+    weight = weight,
+    imageUrl = imageUrl,
+    contractStartDate = contractStartDate,
+    contractEndDate = contractEndDate,
+    positionDetail = mainPosition,
     nationality = nationality
 )

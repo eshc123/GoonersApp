6acc9ecea5f726643f9cd6c3ed655e8758d175d9
Jung SeokJoon <eshc.joon@gmail.com>
2024-01-31 08:14:48 +0900

    add DateUtil methods - getYearAndMonthAndDateLocalDate, getTimeString

--- core/common/src/main/java/com/eshc/goonersapp/core/common/util/DateUtil.kt
+++ core/common/src/main/java/com/eshc/goonersapp/core/common/util/DateUtil.kt
@@ -1,6 +1,7 @@
 package com.eshc.goonersapp.core.common.util
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 const val DefaultDatePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
@@ -11,5 +12,17 @@ object DateUtil {
     fun getYearAndMonthAndDateString(date : String ) : String {
         if(date.isBlank()) return date
         return LocalDate.parse(date, defaultDateFormat).toString()
+    }
+
+    fun getYearAndMonthAndDateLocalDate(date : String ) : LocalDate {
+        if(date.isBlank()) return LocalDate.now()
+        return LocalDate.parse(date, defaultDateFormat)
+    }
+
+    fun getTimeString(date: String) : String {
+        if(date.isBlank()) return date
+        return LocalDateTime.parse(date, defaultDateFormat).let {
+            "${it.hour}:" + "${it.minute}".padStart(2,'0')
+        }
     }
 }

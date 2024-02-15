045547416a7323cf00731b8b2803bff407354137
Jung SeokJoon <eshc.joon@gmail.com>
2024-02-15 23:08:03 +0900

    mod GetMatchesBySeasonUseCase - api, repository ..

--- core/data/src/main/java/com/eshc/goonersapp/core/data/repository/MatchRepositoryImpl.kt
+++ core/data/src/main/java/com/eshc/goonersapp/core/data/repository/MatchRepositoryImpl.kt
@@ -1,15 +1,9 @@
 package com.eshc.goonersapp.core.data.repository
 
-import com.eshc.goonersapp.core.data.mapper.toEntity
 import com.eshc.goonersapp.core.data.mapper.toModel
-import com.eshc.goonersapp.core.database.dao.PlayerDao
 import com.eshc.goonersapp.core.domain.model.Match
-import com.eshc.goonersapp.core.domain.model.Player
 import com.eshc.goonersapp.core.domain.repository.MatchRepository
-import com.eshc.goonersapp.core.domain.repository.PlayerRepository
 import com.eshc.goonersapp.core.network.MatchNetworkDataSource
-import com.eshc.goonersapp.core.network.PlayerNetworkDataSource
-import com.eshc.goonersapp.core.network.remote.MatchNetworkDataSourceImpl
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -18,7 +12,7 @@ class MatchRepositoryImpl @Inject constructor(
     private val matchNetworkDataSource: MatchNetworkDataSource
 ) : MatchRepository {
 
-    override fun getMatchesByMonth(startDate : String, endDate : String): Flow<List<Match>> = flow {
-        emit(matchNetworkDataSource.getMatchesByMonth(startDate,endDate).map { it.toModel() })
+    override fun getMatchesBySeason(season : String): Flow<List<Match>> = flow {
+        emit(matchNetworkDataSource.getMatchesBySeason(season).map { it.toModel() })
     }
 }
--- core/network/src/main/java/com/eshc/goonersapp/core/network/api/MatchNetworkService.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/api/MatchNetworkService.kt
@@ -2,7 +2,6 @@ package com.eshc.goonersapp.core.network.api
 
 import com.eshc.goonersapp.core.network.model.BaseResponse
 import com.eshc.goonersapp.core.network.model.RemoteMatch
-import com.eshc.goonersapp.core.network.model.RemotePlayer
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,9 +10,8 @@ const val MATCH_BASE_URL = "/apis/match"
 interface MatchNetworkService {
 
     @GET(value = "$MATCH_BASE_URL/team")
-    suspend fun getMatchesByMonth(
+    suspend fun getMatchesBySeason(
         @Query("teamId") teamId : Int = 2,
-        @Query("startDate") startDate : String,
-        @Query("endDate") endDate : String,
+        @Query("season") season : String
     ) : Response<BaseResponse<List<RemoteMatch>>>
 }

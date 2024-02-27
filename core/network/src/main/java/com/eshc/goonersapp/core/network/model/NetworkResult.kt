36a1b69e2ec368bceaa632fb9e37509fe344685e
Jung SeokJoon <eshc.joon@gmail.com>
2024-02-28 05:54:36 +0900

    add NetworkResult
    ref) https://proandroiddev.com/modeling-retrofit-responses-with-sealed-classes-and-coroutines-9d6302077dfe

---
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/model/NetworkResult.kt
@@ -0,0 +1,28 @@
+package com.eshc.goonersapp.core.network.model
+
+import retrofit2.HttpException
+import retrofit2.Response
+
+sealed class NetworkResult<T> {
+    class Success<T>(val data: T) : NetworkResult<T>()
+    class Error<T>(val code: Int, val message: String?) : NetworkResult<T>()
+    class Exception<T>(val e: Throwable) : NetworkResult<T>()
+}
+
+suspend fun <T : Any> handleApi(
+    execute: suspend () -> Response<BaseResponse<T>>
+): NetworkResult<T> {
+    return try {
+        val response = execute()
+        val body = response.body()
+        if (response.isSuccessful && body != null) {
+            NetworkResult.Success(body.result)
+        } else {
+            NetworkResult.Error(code = response.code(), message = response.message())
+        }
+    } catch (e: HttpException) {
+        NetworkResult.Error(code = e.code(), message = e.message())
+    } catch (e: Throwable) {
+        NetworkResult.Exception(e)
+    }
+}

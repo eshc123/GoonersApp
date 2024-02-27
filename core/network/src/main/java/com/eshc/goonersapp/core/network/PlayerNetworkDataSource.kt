a750b7bac1de9bd5e28627f40f440e3dc2589e58
Jung SeokJoon <eshc.joon@gmail.com>
2024-02-28 06:21:31 +0900

    mod NetworkResult to PlayerNetworkDataSource

--- core/network/src/main/java/com/eshc/goonersapp/core/network/PlayerNetworkDataSource.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/PlayerNetworkDataSource.kt
@@ -1,10 +1,11 @@
 package com.eshc.goonersapp.core.network
 
+import com.eshc.goonersapp.core.network.model.NetworkResult
 import com.eshc.goonersapp.core.network.model.player.RemotePlayer
 
 interface PlayerNetworkDataSource {
 
-    suspend fun getPlayerList() : List<RemotePlayer>
+    suspend fun getPlayerList() : NetworkResult<List<RemotePlayer>>
 
-    suspend fun getPlayerDetail(playerId : Int) : RemotePlayer
+    suspend fun getPlayerDetail(playerId : Int) : NetworkResult<RemotePlayer>
 }
--- core/network/src/main/java/com/eshc/goonersapp/core/network/remote/PlayerNetworkDataSourceImpl.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/remote/PlayerNetworkDataSourceImpl.kt
@@ -2,6 +2,8 @@ package com.eshc.goonersapp.core.network.remote
 
 import com.eshc.goonersapp.core.network.PlayerNetworkDataSource
 import com.eshc.goonersapp.core.network.api.PlayerNetworkService
+import com.eshc.goonersapp.core.network.model.NetworkResult
+import com.eshc.goonersapp.core.network.model.handleApi
 import com.eshc.goonersapp.core.network.model.player.RemotePlayer
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -11,27 +13,15 @@ class PlayerNetworkDataSourceImpl @Inject constructor(
     private val playerNetworkService: PlayerNetworkService
 ) : PlayerNetworkDataSource {
 
-
-    override suspend fun getPlayerList(): List<RemotePlayer> {
-        return try {
-            val response = playerNetworkService.getPlayers()
-            if(response.isSuccessful){
-                response.body()?.result ?: emptyList()
-            }else emptyList()
-        }catch (e:Exception){
-            emptyList()
+    override suspend fun getPlayerList(): NetworkResult<List<RemotePlayer>> {
+        return handleApi {
+            playerNetworkService.getPlayers()
         }
     }
 
-    override suspend fun getPlayerDetail(playerId: Int): RemotePlayer {
-        return try {
-            val response = playerNetworkService.getPlayerDetail(playerId = playerId)
-            if(response.isSuccessful){
-                response.body()?.result ?: RemotePlayer(-1)
-            }else RemotePlayer(-1)
-        }catch (e:Exception){
-            RemotePlayer(-1)
+    override suspend fun getPlayerDetail(playerId: Int): NetworkResult<RemotePlayer> {
+        return handleApi {
+            playerNetworkService.getPlayerDetail(playerId = playerId)
         }
     }
-
 }

fab285fe7e99553d00197499a75259f924ba1517
Jung SeokJoon <eshc.joon@gmail.com>
2024-02-28 06:07:55 +0900

    mod NetworkResult to MatchNetworkDataSource

--- core/network/src/main/java/com/eshc/goonersapp/core/network/MatchNetworkDataSource.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/MatchNetworkDataSource.kt
@@ -1,12 +1,14 @@
 package com.eshc.goonersapp.core.network
 
+import com.eshc.goonersapp.core.network.model.NetworkResult
 import com.eshc.goonersapp.core.network.model.match.RemoteMatch
+import com.eshc.goonersapp.core.network.model.match.RemoteRecentlyMatch
 
 interface MatchNetworkDataSource {
 
-    suspend fun getMatchesBySeason(season : String) : List<RemoteMatch>
+    suspend fun getMatchesBySeason(season : String) : NetworkResult<List<RemoteMatch>>
 
-    suspend fun getUpcomingMatches() : List<RemoteMatch>
+    suspend fun getUpcomingMatches() : NetworkResult<List<RemoteMatch>>
 
-    suspend fun getRecentlyMatch() : RemoteMatch
+    suspend fun getRecentlyMatch() : NetworkResult<RemoteRecentlyMatch>
 }

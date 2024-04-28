902a680e2c5b20c068db5af1956f6ca931ac2f9a
Jung SeokJoon <eshc.joon@gmail.com>
2024-04-29 08:00:28 +0900

    chore common module
    android -> java/kotlin

---
+++ network/GnrDispathcers.kt
@@ -0,0 +1,15 @@
+package com.eshc.goonersapp.core.common.network
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class MainDispatcher
---
+++ util/DateUtil.kt
@@ -0,0 +1,38 @@
+package com.eshc.goonersapp.core.common.util
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+const val DefaultDatePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+
+object DateUtil {
+    private val defaultDateFormat = DateTimeFormatter.ofPattern(DefaultDatePattern)
+
+    fun getYearAndMonthString(date : String ) : String {
+        if(date.isBlank()) return date
+        return LocalDate.parse(date, defaultDateFormat).format(DateTimeFormatter.ofPattern("yyyy.MM"))
+    }
+
+    fun getYearAndMonthAndDateString(date : String ) : String {
+        if(date.isBlank()) return date
+        return LocalDate.parse(date, defaultDateFormat).format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+    }
+
+    fun getYearAndMonthAndDateAndTimeString(date : String ) : String {
+        if(date.isBlank()) return date
+        return LocalDateTime.parse(date, defaultDateFormat).format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"))
+    }
+
+    fun getYearAndMonthAndDateLocalDate(date : String ) : LocalDate {
+        if(date.isBlank()) return LocalDate.now()
+        return LocalDate.parse(date, defaultDateFormat)
+    }
+
+    fun getTimeString(date: String) : String {
+        if(date.isBlank()) return date
+        return LocalDateTime.parse(date, defaultDateFormat).let {
+            "${it.hour}:" + "${it.minute}".padStart(2,'0')
+        }
+    }
+}

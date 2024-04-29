24073a9a80163348777eca3f3e064cce0a6353cf
kanukim97 <angry_ryan@kakao.com>
2024-04-29 19:27:49 +0900

    [MOD]  - mod HomeScreen, HomeViewModel  - add RecentlyResultUiState, UpcomingMatchesUiState

--- feature/home/src/main/java/com/eshc/goonersapp/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/eshc/goonersapp/feature/home/HomeScreen.kt
@@ -1,17 +1,22 @@
 package com.eshc.goonersapp.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -22,6 +27,8 @@ import com.eshc.goonersapp.core.designsystem.theme.GnrTypography
 import com.eshc.goonersapp.feature.home.component.DashboardCard
 import com.eshc.goonersapp.feature.home.component.RecentlyMatchCard
 import com.eshc.goonersapp.feature.home.component.UpcomingMatchCard
+import com.eshc.goonersapp.feature.home.state.RecentlyResultUiState
+import com.eshc.goonersapp.feature.home.state.UpcomingMatchesUiState
 
 @Composable
 fun HomeRoute(
@@ -30,6 +37,9 @@ fun HomeRoute(
     viewModel: HomeViewModel = hiltViewModel(),
     onShowSnackbar : (String) -> Unit
 ) {
+    val upcomingMatchesUiState by viewModel.upcomingMatchesUiStateFlow.collectAsStateWithLifecycle()
+    val recentlyResultUiState by viewModel.recentlyResultUiStateFlow.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = {
             topBar()
@@ -40,7 +50,8 @@ fun HomeRoute(
     ) { padding ->
         HomeScreen(
             modifier = Modifier.padding(padding),
-            viewModel
+            upcomingMatchesUiState = upcomingMatchesUiState,
+            recentlyResultUiState = recentlyResultUiState
         )
     }
 }
@@ -48,11 +59,9 @@ fun HomeRoute(
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    viewModel: HomeViewModel
+    upcomingMatchesUiState: UpcomingMatchesUiState,
+    recentlyResultUiState: RecentlyResultUiState
 ) {
-    val upcomingMatches by viewModel.upcomingMatches.collectAsStateWithLifecycle()
-    val recentlyMatch by viewModel.recentlyMatch.collectAsStateWithLifecycle()
-
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(top = 12.dp)
@@ -74,54 +83,136 @@ fun HomeScreen(
                 style = GnrTypography.subtitleMedium,
                 color = Color.Black,
             )
-            LazyRow(
-                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 12.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                items(
-                    items = upcomingMatches,
-                    key = { upcomingMatches -> upcomingMatches.id }
-                ) { matches ->
-                    UpcomingMatchCard(
-                        homeUrl = matches.homeTeamImageUrl,
-                        homeShortName = matches.homeTeamNickname,
-                        awayUrl = matches.awayTeamImageUrl,
-                        awayShortName = matches.awayTeamNickname,
-                        time = DateUtil.getYearAndMonthAndDateAndTimeString(matches.matchDate),
-                        location = if (matches.stadiumName == "null") "" else matches.stadiumName,
-                        competitionUrl = matches.leagueImageUrl,
-                        competitionName = "Premier League"
-                    )
+            when (upcomingMatchesUiState) {
+                is UpcomingMatchesUiState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(131.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+                is UpcomingMatchesUiState.Success -> {
+                    LazyRow(
+                        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 12.dp),
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        items(
+                            items = upcomingMatchesUiState.data,
+                            key = { upcomingMatches -> upcomingMatches.id }
+                        ) { matches ->
+                            UpcomingMatchCard(
+                                homeUrl = matches.homeTeamImageUrl,
+                                homeShortName = matches.homeTeamNickname,
+                                awayUrl = matches.awayTeamImageUrl,
+                                awayShortName = matches.awayTeamNickname,
+                                time = DateUtil.getYearAndMonthAndDateAndTimeString(matches.matchDate),
+                                location = if (matches.stadiumName == "null") "" else matches.stadiumName,
+                                competitionUrl = matches.leagueImageUrl,
+                                competitionName = "Premier League"
+                            )
+                        }
+                    }
+                }
+                is UpcomingMatchesUiState.Failed -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(131.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Upcoming Matches cannot be loaded.",
+                            style = MaterialTheme.typography.titleSmall,
+                            color = Color.LightGray,
+                        )
+                    }
+                }
+                is UpcomingMatchesUiState.Error -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(131.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Error! ${upcomingMatchesUiState.throwable}",
+                            style = MaterialTheme.typography.titleSmall,
+                            color = Color.LightGray,
+                        )
+                    }
                 }
             }
         }
 
-        recentlyMatch?.let { match ->
-            item {
-                Text(
-                    modifier = Modifier.padding(start = 8.dp),
-                    text = "Recently Result",
-                    style = GnrTypography.subtitleMedium,
-                    color = Color.Black,
-                )
-                RecentlyMatchCard(
-                    competitionUrl = match.match.leagueImageUrl,
-                    competitionName = "Premier league",
-                    time = DateUtil.getYearAndMonthAndDateAndTimeString(match.match.matchDate),
-                    location = match.match.stadiumName,
-                    homeId = match.match.homeTeamId,
-                    homeUrl = match.match.homeTeamImageUrl,
-                    homeShortName = match.match.homeTeamNickname,
-                    homeScore = match.match.homeScore.toString(),
-                    awayId = match.match.awayTeamId,
-                    awayUrl = match.match.awayTeamImageUrl,
-                    awayShortName = match.match.awayTeamNickname,
-                    awayScore = match.match.awayScore.toString(),
-                    matchHistory = match.matchDetail
-                )
+        item {
+            Text(
+                modifier = Modifier.padding(start = 8.dp),
+                text = "Recently Result",
+                style = GnrTypography.subtitleMedium,
+                color = Color.Black,
+            )
+            when (recentlyResultUiState) {
+                is RecentlyResultUiState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(273.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+                is RecentlyResultUiState.Success -> {
+                    recentlyResultUiState.data?.let { match ->
+                        RecentlyMatchCard(
+                            competitionUrl = match.match.leagueImageUrl,
+                            competitionName = "Premier league",
+                            time = DateUtil.getYearAndMonthAndDateAndTimeString(match.match.matchDate),
+                            location = match.match.stadiumName,
+                            homeId = match.match.homeTeamId,
+                            homeUrl = match.match.homeTeamImageUrl,
+                            homeShortName = match.match.homeTeamNickname,
+                            homeScore = match.match.homeScore.toString(),
+                            awayId = match.match.awayTeamId,
+                            awayUrl = match.match.awayTeamImageUrl,
+                            awayShortName = match.match.awayTeamNickname,
+                            awayScore = match.match.awayScore.toString(),
+                            matchHistory = match.matchDetail
+                        )
+                    }
+                }
+                is RecentlyResultUiState.Failed -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(273.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Recently Result cannot be loaded.",
+                            style = MaterialTheme.typography.titleSmall,
+                            color = Color.LightGray,
+                        )
+                    }
+                }
+                is RecentlyResultUiState.Error -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(273.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Error! ${recentlyResultUiState.throwable}",
+                            style = MaterialTheme.typography.titleSmall,
+                            color = Color.LightGray,
+                        )
+                    }
+                }
             }
         }
-
     }
 }
-

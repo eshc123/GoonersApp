ed0c47075a748d1120f44c0d0a3fd8663b092043
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-04 09:50:57 +0900

    chore clear unused import

--- feature/team/src/main/java/com/eshc/goonersapp/feature/team/history/TeamHistoryScreen.kt
+++ feature/team/src/main/java/com/eshc/goonersapp/feature/team/history/TeamHistoryScreen.kt
@@ -2,11 +2,8 @@ package com.eshc.goonersapp.feature.team.history
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.eshc.goonersapp.core.designsystem.component.LargeDropdownMenu
 import com.eshc.goonersapp.core.designsystem.component.TopBar
 
 @Composable

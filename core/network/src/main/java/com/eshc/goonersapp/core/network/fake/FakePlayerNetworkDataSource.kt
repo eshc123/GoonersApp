b9df248a53cc5cb4e19e2f6744c10687bd0d43ab
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-04 13:28:02 +0900

    mod FakePlayerNetworkDataSource

--- core/network/src/main/java/com/eshc/goonersapp/core/network/fake/FakePlayerNetworkDataSource.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/fake/FakePlayerNetworkDataSource.kt
@@ -2,26 +2,38 @@ package com.eshc.goonersapp.core.network.fake
 
 import com.eshc.goonersapp.core.network.PlayerNetworkDataSource
 import com.eshc.goonersapp.core.network.model.NetworkResult
+import com.eshc.goonersapp.core.network.model.match.RemoteMatch
+import com.eshc.goonersapp.core.network.model.match.RemoteMatchData
 import com.eshc.goonersapp.core.network.model.player.RemotePlayer
 
 class FakePlayerNetworkDataSource : PlayerNetworkDataSource {
 
-    private var responseForPlayerList : (() -> NetworkResult<List<RemotePlayer>>)? = null
-    private var responseForPlayerDetail : (() -> NetworkResult<RemotePlayer>)? = null
+    private var responseForPlayerList: (() -> List<RemotePlayer>)? = null
+    private var responseForPlayerDetail: (() -> RemotePlayer)? = null
 
-    fun setResponseForPlayerList(response: () -> NetworkResult<List<RemotePlayer>>) {
+    fun setResponseForPlayerList(response: () -> List<RemotePlayer>) {
         this.responseForPlayerList = response
     }
 
-    fun setResponseForPlayerDetail(response: () -> NetworkResult<RemotePlayer>) {
+    fun setResponseForPlayerDetail(response: () -> RemotePlayer) {
         this.responseForPlayerDetail = response
     }
 
     override suspend fun getPlayerList(): NetworkResult<List<RemotePlayer>> {
-        return responseForPlayerList?.invoke() ?: throw IllegalStateException("Response for getPlayerList not set")
+        val response = responseForPlayerList?.invoke()
+        return if (response != null) {
+            NetworkResult.Success(response)
+        } else {
+            NetworkResult.Error(code = 404, message = "Response for getPlayerList not se")
+        }
     }
 
     override suspend fun getPlayerDetail(playerId: Int): NetworkResult<RemotePlayer> {
-        return responseForPlayerDetail?.invoke() ?: throw IllegalStateException("Response for getPlayerDetail not set")
+        val response = responseForPlayerDetail?.invoke()
+        return if (response != null) {
+            NetworkResult.Success(response)
+        } else {
+            NetworkResult.Error(code = 404, message = "Response for getPlayerList not se")
+        }
     }
 }

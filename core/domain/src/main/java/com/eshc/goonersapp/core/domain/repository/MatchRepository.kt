b26de270fe8a34c5265d4bffdff1cf3452cfb14d
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-05 22:02:51 +0900

    add mod MatchRepository & GetRecentlyMatchUseCase

--- core/domain/src/main/java/com/eshc/goonersapp/core/domain/repository/MatchRepository.kt
+++ core/domain/src/main/java/com/eshc/goonersapp/core/domain/repository/MatchRepository.kt
@@ -2,13 +2,13 @@ package com.eshc.goonersapp.core.domain.repository
 
 import com.eshc.goonersapp.core.domain.model.DataResult
 import com.eshc.goonersapp.core.domain.model.match.Match
+import com.eshc.goonersapp.core.domain.model.match.MatchData
 import com.eshc.goonersapp.core.domain.model.match.MatchInformation
-import com.eshc.goonersapp.core.domain.model.match.MatchRecently
 import kotlinx.coroutines.flow.Flow
 
 interface MatchRepository {
 
-    fun getMatch(match: Int): Flow<DataResult<Match>>
+    fun getMatch(matchId: Int): Flow<DataResult<MatchData>>
 
     fun getMatchInformation(match: Int, season: Int, opponent: Int): Flow<DataResult<MatchInformation>>
 
@@ -16,6 +16,6 @@ interface MatchRepository {
 
     fun getUpcomingMatches() : Flow<DataResult<List<Match>>>
 
-    fun getRecentlyMatch() : Flow<DataResult<MatchRecently>>
+    fun getRecentlyMatch() : Flow<DataResult<MatchData>>
 
 }

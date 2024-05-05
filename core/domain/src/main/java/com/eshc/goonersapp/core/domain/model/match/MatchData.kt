2a24bc805c9083b93c622950b1c426749a0d5a39
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-05 22:02:23 +0900

    add MatchData & GetMatchDetailUseCase

---
+++ core/domain/src/main/java/com/eshc/goonersapp/core/domain/model/match/MatchData.kt
@@ -0,0 +1,6 @@
+package com.eshc.goonersapp.core.domain.model.match
+
+data class MatchData(
+    val match: Match,
+    val matchDetail: List<MatchDetail>
+)
---
+++ core/domain/src/main/java/com/eshc/goonersapp/core/domain/usecase/match/GetMatchDetailUseCase.kt
@@ -0,0 +1,17 @@
+package com.eshc.goonersapp.core.domain.usecase.match
+
+import com.eshc.goonersapp.core.domain.model.DataResult
+import com.eshc.goonersapp.core.domain.model.match.MatchData
+import com.eshc.goonersapp.core.domain.repository.MatchRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMatchDetailUseCase @Inject constructor(
+    private val matchRepository: MatchRepository
+) {
+    operator fun invoke(
+        matchId: Int,
+    ): Flow<DataResult<MatchData>> = matchRepository.getMatch(
+        matchId
+    )
+}

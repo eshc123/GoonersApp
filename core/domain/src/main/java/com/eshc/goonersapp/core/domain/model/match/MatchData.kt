11c7748f3f065ed461867b9b8b88cf4acdc08ea7
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-06 10:28:48 +0900

    add getScoreHistoryList in MatchData

--- core/domain/src/main/java/com/eshc/goonersapp/core/domain/model/match/MatchData.kt
+++ core/domain/src/main/java/com/eshc/goonersapp/core/domain/model/match/MatchData.kt
@@ -3,4 +3,9 @@ package com.eshc.goonersapp.core.domain.model.match
 data class MatchData(
     val match: Match,
     val matchDetail: List<MatchDetail>
-)
+){
+    fun getScoreHistoryList(teamId: Int? = null) =
+        matchDetail.filter {
+            it.isScoredMatchDetailType && (teamId == null || it.teamId == teamId)
+        }
+}

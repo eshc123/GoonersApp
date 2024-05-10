df13c34c684e2ec6d9a11850b2243f04c8c38323
Jung SeokJoon <eshc.joon@gmail.com>
2024-05-10 21:28:12 +0900

    add FootballFieldBox

---
+++ feature/match/src/main/java/com/eshc/goonersapp/feature/match/component/formation/FootballFieldBox.kt
@@ -0,0 +1,164 @@
+package com.eshc.goonersapp.feature.match.component.formation
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.eshc.goonersapp.core.designsystem.theme.BrushMainGradient
+import com.eshc.goonersapp.core.designsystem.theme.Color88FFFFFF
+
+@Composable
+fun FootballFieldBox(
+    modifier: Modifier = Modifier,
+    rotateDegree : Float = 0f
+) {
+    var firstCircle by remember { mutableStateOf(Pair(0.5f, 0.125f)) }
+    var boxSize by remember { mutableStateOf(Triple<Float, Float, Boolean>(0f, 0f, true)) }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(10.dp))
+            .background(BrushMainGradient)
+            .rotate(rotateDegree),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = modifier.onGloballyPositioned {
+                boxSize = Triple(
+                    it.size.width.toFloat(),
+                    it.size.height.toFloat(),
+                    boxSize.second > it.size.height.toFloat()
+                )
+            }
+        ) {
+            drawArc(
+                color = Color88FFFFFF,
+                startAngle = 0f,
+                sweepAngle = 180f,
+                useCenter = false,
+                style = Stroke(width = 3f),
+                size = Size(width = size.width * 0.3f, height = size.height * 0.3f),
+                topLeft = Offset(size.width * 0.35f, (size.height * 0.15f) * -1f)
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(0f, 0f),
+                end = Offset(0f, size.height),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(0f, size.height),
+                end = Offset(size.width, size.height),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width, size.height),
+                end = Offset(size.width, 0f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width, 0f),
+                end = Offset(0f, 0f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.3f, size.height),
+                end = Offset(size.width * 0.3f, size.height * 0.84f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.3f, size.height * 0.84f),
+                end = Offset(size.width * 0.7f, size.height * 0.84f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.7f, size.height * 0.84f),
+                end = Offset(size.width * 0.7f, size.height),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.4f, size.height),
+                end = Offset(size.width * 0.4f, size.height * 0.9f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.4f, size.height * 0.9f),
+                end = Offset(size.width * 0.6f, size.height * 0.9f),
+                strokeWidth = 3f
+            )
+            drawLine(
+                color = Color88FFFFFF,
+                start = Offset(size.width * 0.6f, size.height * 0.9f),
+                end = Offset(size.width * 0.6f, size.height),
+                strokeWidth = 3f
+            )
+        }
+        Box(
+            modifier = Modifier
+                .width(boxSize.first.dp)
+                .height(boxSize.second.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .offset(
+                        (boxSize.first * firstCircle.first).dp - 25.dp,
+                        (boxSize.second * firstCircle.second).dp
+                    )
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color.Cyan)
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            val nextX =
+                                ((boxSize.first * firstCircle.first) + dragAmount.x) / boxSize.first
+                            val nextY =
+                                ((boxSize.second * firstCircle.second) + dragAmount.y) / boxSize.second
+                            firstCircle = Pair(nextX, nextY)
+                        }
+                    }
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewFormationBox(){
+    FootballFieldBox()
+}

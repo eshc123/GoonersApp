3f6c14c78775644bdc79ee21debc63c3cbfa7f1a
Jung SeokJoon <eshc.joon@gmail.com>
2024-06-01 10:12:18 +0900

    add getMonthAndDateAndDayString in DataUtil

--- core/common/src/main/java/com/eshc/goonersapp/core/common/util/DateUtil.kt
+++ core/common/src/main/java/com/eshc/goonersapp/core/common/util/DateUtil.kt
@@ -29,6 +29,11 @@ object DateUtil {
         return LocalDateTime.parse(date, defaultDateFormat).format(DateTimeFormatter.ofPattern("yyyy.MM.dd E HH:mm")).uppercase()
     }
 
+    fun getMonthAndDateAndDayString(date : String ) : String {
+        if(date.isBlank()) return date
+        return LocalDateTime.parse(date, defaultDateFormat).format(DateTimeFormatter.ofPattern("MM.dd E")).uppercase()
+    }
+
     fun getYearAndMonthAndDateLocalDate(date : String ) : LocalDate {
         if(date.isBlank()) return LocalDate.now()
         return LocalDate.parse(date, defaultDateFormat)

32657ca48c8d804618ce147704221cda09ef9c5e
Jung SeokJoon <eshc.joon@gmail.com>
2024-06-07 21:29:48 +0900

    mod getPlayers param & response

--- core/network/src/main/java/com/eshc/goonersapp/core/network/api/PlayerNetworkService.kt
+++ core/network/src/main/java/com/eshc/goonersapp/core/network/api/PlayerNetworkService.kt
@@ -2,6 +2,7 @@ package com.eshc.goonersapp.core.network.api
 
 import com.eshc.goonersapp.core.network.model.BaseResponse
 import com.eshc.goonersapp.core.network.model.player.RemotePlayer
+import com.eshc.goonersapp.core.network.model.player.RemotePlayerList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,9 +15,10 @@ interface PlayerNetworkService {
         @Query("teamId") teamId : Int = 19,
         @Query("seasonId") seasonId : Int = 21646,
         @Query("positionId") positionId : Int? = null,
-        @Query("keyword") keyword : String? = null
-
-    ) : Response<BaseResponse<List<RemotePlayer>>>
+        @Query("keyword") keyword : String? = null,
+        @Query("page") page : Int? = null,
+        @Query("size") size : Int? = null
+    ) : Response<BaseResponse<RemotePlayerList>>
 
     @GET(value = "$PLAYER_BASE_URL/detail")
     suspend fun getPlayerDetail(
